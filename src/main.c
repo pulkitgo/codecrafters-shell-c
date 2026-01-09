a57b6feae7720f967fd29e90c3c8d83698a6e678
pulkitgo <Pulkit.Goyal@iiitb.ac.in>
2026-01-09 21:08:55 +0530

    9:08 , 9JAN

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-  // char s[100];
-  // scanf("%s",&s);
+  char s[100];
+  scanf("%s",&s);
   // s=argv;
   // TODO: Uncomment the code below to pass the first stage
   if(*argv=="$ ")
