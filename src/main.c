91500638b2221e163ec9cce30c79c514f7debfee
pulkitgo <Pulkit.Goyal@iiitb.ac.in>
2026-01-09 21:15:40 +0530

    9:15 , 9JAN

--- src/main.c
+++ src/main.c
@@ -5,10 +5,11 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   char s[100];
+  printf("$ ");
+  
   scanf("%99s",&s);
   // s=argv;
   // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
   printf("%s: command not found\n", s);
   return 0;
 }
