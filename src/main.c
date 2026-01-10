630663ab1ff853bcc59329c4aa0bba085987d73a
pulkitgo <Pulkit.Goyal@iiitb.ac.in>
2026-01-10 11:38:18 +0530

    11:38 10Jan

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -10,7 +10,9 @@ int main(int argc, char *argv[]) {
     printf("$ ");
   
     scanf("%99s",&s);
-    if(s=="exit")
+    // printf("%d",strlen(s));
+    // s[strlen(s)]='\0';
+    if(strcmp(s,"exit")==0)
       break;
     printf("%s: command not found\n", s);
   }
