dcd3db556fa873f179a3030940c5177e7cae1b58
pulkitgo <Pulkit.Goyal@iiitb.ac.in>
2026-01-10 11:19:25 +0530

    11:19 , 10JAN

--- src/main.c
+++ src/main.c
@@ -5,11 +5,14 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   char s[100];
-  printf("$ ");
+  while(1)
+  {
+    printf("$ ");
   
-  scanf("%99s",&s);
-  // s=argv;
-  // TODO: Uncomment the code below to pass the first stage
-  printf("%s: command not found\n", s);
+    scanf("%99s",&s);
+    // s=argv;
+    // TODO: Uncomment the code below to pass the first stage
+    printf("%s: command not found\n", s);
+  }
   return 0;
 }
